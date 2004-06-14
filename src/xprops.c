124fcdfca951c4955d6ee8ca1ff86eda35b22006
Havoc Pennington <hp@redhat.com>
2004-06-14 19:28:23 +0000

    fix this to compile (1 year later) - clearly everyone is testing GNOME 2.2
    2004-06-14  Havoc Pennington  <hp@redhat.com>
    
    	* src/xprops.c (utf8_list_from_results): fix this to compile (1
    	year later) - clearly everyone is testing GNOME 2.2 from CVS...

--- src/xprops.c
+++ src/xprops.c
@@ -459,7 +459,7 @@ utf8_list_from_results (GetPropertyResults *results,
       ++i;
     }
 
-  if (results->prop[n_items - 1] != '\0')
+  if (results->prop[results->n_items - 1] != '\0')
     ++n_strings;
 
   /* we're guaranteed that results->prop has a nul on the end
