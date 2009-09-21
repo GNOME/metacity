11f1dcc18d1e560485eb21ab6a2b69a3b3975e6e
Thomas Thurman <tthurman@gnome.org>
2009-09-20 22:39:44 -0400

    copyright

--- src/ui/theme.h
+++ src/ui/theme.h
@@ -4,6 +4,7 @@
 
 /* 
  * Copyright (C) 2001 Havoc Pennington
+ * Copyright (C) 2009 Thomas Thurman
  * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
