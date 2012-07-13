dac516955b23f070e1a50cc1f356278b89eb2705
Jasper St. Pierre <jstpierre@mecheye.net>
2012-07-13 18:17:43 -0400

    workspace: respect the not_this_one parameter passed in
    In the case of focus-follows-mouse, we need to ensure that we
    do not select a certain window after closing another one.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=675982

--- src/core/workspace.c
+++ src/core/workspace.c
@@ -994,7 +994,7 @@ focus_ancestor_or_top_window (MetaWorkspace *workspace,
     }
 
   window = meta_stack_get_default_focus_window (workspace->screen->stack,
-                                                workspace, NULL);
+                                                workspace, not_this_one);
 
   if (window)
     {
