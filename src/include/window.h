542cc51dd6483a5eb3baa98b7ddcf7688cc14ac3
Davyd Madeley <davyd@madeley.id.au>
2014-09-27 17:40:10 +0300

    Add meta_window_get_transient_for() method
    NOTE: Patch copied from mutter and adapted for metacity.

--- src/include/window.h
+++ src/include/window.h
@@ -33,5 +33,6 @@ MetaRectangle *meta_window_get_rect (MetaWindow *window);
 MetaScreen *meta_window_get_screen (MetaWindow *window);
 MetaDisplay *meta_window_get_display (MetaWindow *window);
 Window meta_window_get_xwindow (MetaWindow *window);
+MetaWindow *meta_window_get_transient_for (MetaWindow *window);
 
 #endif
