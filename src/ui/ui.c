aa16325e8b938ca6d471fdf508a7276e577f8212
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2014-11-24 14:02:07 +0200

    ui: use correct name for missing icon

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -596,7 +596,7 @@ meta_ui_get_default_window_icon (MetaUI *ui)
                                                    NULL);
       else
           default_icon = gtk_icon_theme_load_icon (theme,
-                                                   "gtk-missing-image",
+                                                   "image-missing",
                                                    META_ICON_WIDTH,
                                                    0,
                                                    NULL);
@@ -631,7 +631,7 @@ meta_ui_get_default_mini_icon (MetaUI *ui)
                                                    NULL);
       else
           default_icon = gtk_icon_theme_load_icon (theme,
-                                                   "gtk-missing-image",
+                                                   "image-missing",
                                                    META_MINI_ICON_WIDTH,
                                                    0,
                                                    NULL);
