0baa5d7bdeedab5b5cebc29c762ecb34fca22f7b
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2017-07-18 21:26:21 +0300

    libmetacity: fix copy/paste errors

--- libmetacity/meta-theme-gtk.c
+++ libmetacity/meta-theme-gtk.c
@@ -407,9 +407,9 @@ meta_theme_gtk_calc_geometry (MetaThemeImpl     *impl,
           if (button->visible)
             {
               if (button->type != META_BUTTON_TYPE_SPACER)
-                n_left++;
+                n_right++;
               else
-                n_left_spacers++;
+                n_right_spacers++;
             }
         }
     }
