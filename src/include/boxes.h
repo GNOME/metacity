922de13cbd51b38110cf3571131aef6ca6fa6a3a
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2020-01-30 16:48:28 +0200

    window: add support for _GNOME_WM_STRUT_AREA
    In addition to existing properties use also new _GNOME_WM_STRUT_AREA
    property that allows creating struts between monitors.
    
    https://mail.gnome.org/archives/wm-spec-list/2018-December/msg00000.html
    https://gitlab.freedesktop.org/xdg/xdg-specs/merge_requests/22

--- src/include/boxes.h
+++ src/include/boxes.h
@@ -25,6 +25,13 @@
 #include <glib.h>
 #include "common.h"
 
+typedef enum
+{
+  META_EDGE_WINDOW,
+  META_EDGE_MONITOR,
+  META_EDGE_SCREEN
+} MetaEdgeType;
+
 typedef struct _MetaRectangle MetaRectangle;
 struct _MetaRectangle
 {
@@ -39,6 +46,7 @@ struct _MetaStrut
 {
   MetaRectangle rect;
   MetaSide side;
+  MetaEdgeType edge;
 };
 
 #define BOX_LEFT(box)    ((box).x)                /* Leftmost pixel of rect */
@@ -52,13 +60,6 @@ typedef enum
   FIXED_DIRECTION_X    = 1 << 0,
   FIXED_DIRECTION_Y    = 1 << 1,
 } FixedDirections;
-
-typedef enum
-{
-  META_EDGE_WINDOW,
-  META_EDGE_MONITOR,
-  META_EDGE_SCREEN
-} MetaEdgeType;
 
 typedef struct _MetaEdge MetaEdge;
 struct _MetaEdge
@@ -156,7 +157,8 @@ void meta_rectangle_resize_with_gravity (const MetaRectangle *old_rect,
  */
 GList*   meta_rectangle_get_minimal_spanning_set_for_region (
                                          const MetaRectangle *basic_rect,
-                                         const GSList        *all_struts);
+                                         const GSList        *all_struts,
+                                         gboolean             skip_middle_struts);
 
 /* Expand all rectangles in region by the given amount on each side */
 GList*   meta_rectangle_expand_region   (GList               *region,
